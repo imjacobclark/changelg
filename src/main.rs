5f949aa16b5fcbeeda6acdf7fa934be98a0f9b51
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-06-02 23:21:26 +0100

    Refactoring and testing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 extern crate git2;
 extern crate regex;
 
-use std::env;
-use git2::Repository;
+#[cfg(not(test))] use std::env;
+
+use git2::{Repository, Commit};
 use regex::Regex;
 
-macro_rules! filter_try {
+macro_rules! filter_option {
     ($e:expr) => (match $e { Ok(t) => t, Err(e) => return Some(Err(e)) })
 }
 
@@ -14,9 +15,35 @@ macro_rules! option_match {
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
     let repo = option_match!(Repository::open("./"));
     let mut revwalk = option_match!(repo.revwalk());
+
+    option_match!(revwalk.push_range(&commit_range()[..]));
+
+    let revwalk = revwalk.filter_map(|id| {
+        let id = option_match!(id);
+        let commit = filter_option!(repo.find_commit(id));        
+        Some(Ok(commit))
+    });
+    
+    for commit in revwalk {
+        let commit = option_match!(commit);
+        print_to_stdout(&commit);
+    }
+}
+
+fn print_to_stdout(commit: &Commit) {            
+    for line in String::from_utf8_lossy(commit.message_bytes()).lines() {
+        let re = Regex::new(r"^#changelog.*$").unwrap();
+        
+        if re.is_match(line) {
+            println!("{} by {}", line, commit.author());
+        }
+    };        
+}
+
+fn commit_range() -> String {
+    let args: Vec<_> = env::args().collect();
     let commit_from: &str = &args[1][..];
     let commit_to: &str = &args[2][..];
     let mut commit_range = String::from("");
@@ -25,23 +52,35 @@ fn main() {
     commit_range.push_str("..");
     commit_range.push_str(commit_to);
     
-    option_match!(revwalk.push_range(&commit_range[..]));
+    commit_range
+}
 
-    let revwalk = revwalk.filter_map(|id| {
-        let id = option_match!(id);
-        let commit = filter_try!(repo.find_commit(id));        
-        Some(Ok(commit))
-    });
-    
-    for commit in revwalk {
-        let commit = option_match!(commit);
-                
-        for line in String::from_utf8_lossy(commit.message_bytes()).lines() {
-            let re = Regex::new(r"^#changelog.*$").unwrap();
-            
-            if re.is_match(line) {
-                    println!("{} by {}", line, commit.author());
-            }
-        }        
+#[cfg(test)] mod env {
+    use std::vec;
+    pub fn args() -> vec::IntoIter<&'static str> { vec!["changelg", "377d686351969f27f288dec2fb09d0d5431fcde1", "3763e0e3ff218cbdfbf99c68109a04d666e81abeto"].into_iter() }
+}
+
+#[test]
+#[should_panic]
+fn it_should_panic_on_error_in_option_match() {
+    fn something_bad_happens<T>() -> Result<T, &'static str> {
+        Err("Fail")    
     };
+    
+    option_match!(something_bad_happens());
+}
+
+#[test]
+fn it_should_return_result_on_ok_in_option_match() {
+    fn something_good_happens() -> Result<&'static str, &'static str> {
+        Ok("Good")    
+    };
+    
+    let result = option_match!(something_good_happens());
+    assert_eq!(result, "Good");
+}
+
+#[test]
+fn it_should_return_expected_commit_range_string(){
+    assert_eq!(commit_range(), "377d686351969f27f288dec2fb09d0d5431fcde1..3763e0e3ff218cbdfbf99c68109a04d666e81abeto");
 }
